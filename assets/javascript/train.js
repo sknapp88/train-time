66857dfdd466fe434e70c616fec02eb89e74267b
sknapp88 <knapp533@gmail.com>
2017-04-05 18:01:03 -0400

    almost there

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -28,48 +28,39 @@
       firstTime: firstTime,
       freq: freq
     });
-    console.log(name);
 
     database.ref().on("child_added", function(childSnapshot, prevChildKey){
-      console.log(childSnapshot.val());
+
       var trainName = childSnapshot.val().name;
       var trainDestination = childSnapshot.val().destination;
       var trainStart = childSnapshot.val().firstTime;
       var trainFreq = childSnapshot.val().freq;
 
-      console.log(trainName);
-      console.log(trainDestination);
-      console.log(trainStart);
-      console.log(trainFreq);
-
       var startTime = moment(trainStart, "hh:mm");
       var currentTime = moment().format("hh:mm");
       var diffTime = moment().diff(moment(startTime), "minutes");
-      console.log(diffTime);
       var waitTime = diffTime % trainFreq;
       var minutesTT = trainFreq - waitTime;
-      var nextTrain = moment().add(minutesTT, "hh:mm");
+      var nextTrain = moment().add(waitTime, "minutes");
 
 
 
       $("#on-time").append(`
-          <tr>
-            <td id="nameDisplay">${trainName}</td>
-            <td id="trainDestination">${childSnapshot.val().destination}</td>
-            <td id="trainFreq">${childSnapshot.val().freq}</td>
-            <td id="arrTime">${nextTrain}</td>
-            <td id="minutes">${minutesTT}</td>
-          </tr>
+        <tr>
+        <td id="nameDisplay">${trainName}</td>
+        <td id="trainDestination">${childSnapshot.val().destination}</td>
+        <td id="trainFreq">${childSnapshot.val().freq}</td>
+        <td id="arrTime">${moment(nextTrain).format("hh:mm")}</td>
+        <td id="minutes">${waitTime}</td>
+        </tr>
 
         `);
       $("#name").val('');
       $("#destination").val('');
       $("#first-time").val('');
       $("#freq").val('');
+
+      return false;
     });
-
-
-
-
 
   });
